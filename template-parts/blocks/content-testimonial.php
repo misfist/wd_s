0a786b632b5dab333278faf4e00880111141628d
Greg Rickaby <gregrickaby@gmail.com>
2018-10-15 09:14:00 -0500

    fix up testimonial block

--- template-parts/blocks/content-testimonial.php
+++ template-parts/blocks/content-testimonial.php
@@ -11,19 +11,25 @@ $avatar      = get_field( 'avatar' );
 $testimonial = get_field( 'testimonial' );
 $bg_color    = get_field( 'background_color' );
 $text_color  = get_field( 'text_color' );
-$block_id    = 'testimonial-' . $block['id'];
-
 ?>
-<blockquote id="<?php echo esc_attr( $block_id ); ?>" class="testimonial">
-	<?php wp_kses_post( $testimonial ); ?>
+
+<blockquote class="testimonial">
+	<p><?php echo wp_kses_post( $testimonial ); ?></p>
 	<cite>
-		<img src="<?php echo esc_url( $avatar['url'] ); ?>" alt="<?php echo esc_attr( $avatar['alt'] ); ?>" />
-		<span><?php echo esc_html( $author ); ?></span>
+		<img class="avatar" src="<?php echo esc_url( $avatar ); ?>" alt="<?php echo esc_attr( $author ); ?>" />
+		<span class="author-name"><?php echo esc_html( $author ); ?></span>
 	</cite>
 </blockquote>
 <style type="text/css">
-	#<?php echo esc_attr( $id ); ?> {
+	.testimonial {
 		background: <?php echo esc_attr( $bg_color ); ?>;
 		color: <?php echo esc_attr( $text_color ); ?>;
+		padding: 24px;
+	}
+
+	.testimonial .avatar {
+		border-radius: 50%;
+		height: 64px;
+		width: 64px;
 	}
 </style>
