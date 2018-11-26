2b29954db3fa27507bde1da5ab2f9e402684b165
jomurgel <dev@jomurgel.com>
2018-11-26 14:25:19 -0700

    updates _s_posted_on()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -9,7 +9,7 @@
 
 if ( ! function_exists( '_s_posted_on' ) ) :
 	/**
-	 * Prints HTML with meta information for the current post-date/time and author.
+	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function _s_posted_on() {
 		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
@@ -17,20 +17,23 @@ if ( ! function_exists( '_s_posted_on' ) ) :
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 		}
 
-		$time_string = sprintf(
-			$time_string,
-			esc_attr( get_the_date( 'c' ) ),
+		$time_string = sprintf( $time_string,
+			esc_attr( get_the_date( DATE_W3C ) ),
 			esc_html( get_the_date() ),
-			esc_attr( get_the_modified_date( 'c' ) ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
 			esc_html( get_the_modified_date() )
 		);
 
 		$posted_on = sprintf(
-			/* translators: the date the post was published */
+			/* translators: %s: post date. */
 			esc_html_x( 'Posted on %s', 'post date', '_s' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
+
+	}
+endif;
 		$byline = sprintf(
 			/* translators: the post author */
 			esc_html_x( 'by %s', 'post author', '_s' ),
