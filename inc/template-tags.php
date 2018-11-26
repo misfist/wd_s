70182c013a4ebc14da04b2a780794e8dd75b2719
jomurgel <dev@jomurgel.com>
2018-11-26 14:25:45 -0700

    Updates _s_entry_footer()

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -60,30 +60,50 @@ if ( ! function_exists( '_s_entry_footer' ) ) :
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( esc_html__( ', ', '_s' ) );
-			if ( $categories_list && _s_categorized_blog() ) {
-				/* translators: the post category */
+			if ( $categories_list ) {
+				/* translators: 1: list of categories. */
 				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', '_s' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html__( ', ', '_s' ) );
+			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', '_s' ) );
 			if ( $tags_list ) {
-				/* translators: the post tags */
+				/* translators: 1: list of tags. */
 				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', '_s' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 			}
 		}
 
 		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
-			comments_popup_link( esc_html__( 'Leave a comment', '_s' ), esc_html__( '1 Comment', '_s' ), esc_html__( '% Comments', '_s' ) );
+			comments_popup_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: post title */
+						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', '_s' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				)
+			);
 			echo '</span>';
 		}
 
 		edit_post_link(
 			sprintf(
-				/* translators: %s: Name of current post */
-				esc_html__( 'Edit %s', '_s' ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', '_s' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
 			),
 			'<span class="edit-link">',
 			'</span>'
